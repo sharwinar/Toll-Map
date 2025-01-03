b45a8172c3b3d54292e741ca314b13be44914607
sharwinar <sharwin.a@blackbuck.com>
2025-01-03 14:23:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,6 +93,5 @@ def get_regions():
 # Run the Flask app
 import os
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))  # Get the PORT from the environment, default to 5000
     app.run(host='0.0.0.0', port=port, debug=True)
